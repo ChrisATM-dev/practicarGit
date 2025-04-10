58d23628ce516e355feb6f924c58fcf6e970da45
Christian Torres <Christian.atm24@gmail.com>
2025-04-09 23:11:40 -0400

    base de funciones

--- src/main/java/OperacionesBasicas.java
+++ src/main/java/OperacionesBasicas.java
@@ -1,6 +1,18 @@
 public class OperacionesBasicas {
 
     public static Integer suma(int a, int b){
-        return a + b;
+        return 0;
+    }
+
+    public static Integer resta(int a, int b){
+        return 0;
+    }
+
+    public static Integer multiplicacion(int a, int b){
+        return 0;
+    }
+
+    public static Double division(int a, int b){
+        return 0.0;
     }
 }
